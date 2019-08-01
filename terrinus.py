fca3b7379d2e21f2c17aa8cd1adb1ff17ee0046a
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2019-08-01 16:38:09 +0200

    Added terrinus' skeleton

--- terrinus.py
+++ terrinus.py
@@ -3,9 +3,30 @@ import numpy as np
 
 
 class Terrinus:
+    
+    
     def __init__(self, camera, processing_unit, mapper, controller):
         self.camera = camera
         self.processing_unit = processing_unit
         self.mapper = mapper
         self.controller = controller
-        
+    
+    
+    def detect(self):
+        mask = self.processing_unit.segment()
+
+        return mask
+    
+    
+    def create_map(self):
+        map = None
+        return map
+    
+    
+    def compute_trajectory(self):
+        trajectory = None
+        return trajectory
+    
+
+    def compute_control(self):
+        return (power, steering)
