56e9afcfd04e4c5a79b805748657651b910d1ac8
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2020-10-21 14:31:36 -0700

    Added final code

--- sspytorch/prova_def_2.py
+++ sspytorch/prova_def_2.py
@@ -23,68 +23,8 @@ from jetcam.csi_camera import CSICamera
 import board
 import busio
 
-# print("Aneu desconectant")
-# for j in range(40):
-#     print(40-j)
-#     time.sleep(1)
 
-torch.cuda.set_device(0)
-
-cam = Camera()
-grid_params = {'xw':0.6, 'yw':+0.25, 'length_x':1.0, 'length_y':0.50, 'num_x':7,'num_y':7}
-geompu = GeomPU(cam.get_K(), cam.get_Rt(),grid_params)
-
-image = cam.get_frame_cv2()
-image_path = "/home/dlinano/Pictures/Terrinus/16.jpeg"
-image = cv2.imread(image_path)
-cv2.imwrite('/home/dlinano/Pictures/pic1.jpeg', image)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-img = Image.fromarray(image)
-ori_width, ori_height = img.size
-
-# image transform, to torch float tensor 3xHxW
-img = imresize(img, (480,640))
-img = img_transform(img)
-img = torch.unsqueeze(img, 0)
-#print("img size = " + str(img.size()))
-
-with torch.no_grad():
-    orig_mobilenet = mobilenet.__dict__['mobilenetv2'](pretrained=False)
-    net_encoder = MobileNetV2Dilated(orig_mobilenet, dilate_scale=8)
-    enc_weights = "/home/dlinano/maweights/ade20k-mobilenetv2dilated-c1_deepsup/encoder_epoch_20.pth"
-    net_encoder.load_state_dict(torch.load(enc_weights, map_location='cuda:0'), strict=False)
-    
-    net_decoder = C1DeepSup(
-                num_class=150,
-                fc_dim=320,
-                use_softmax=True)
-    dec_weights = "/home/dlinano/maweights/ade20k-mobilenetv2dilated-c1_deepsup/decoder_epoch_20.pth"
-    net_decoder.load_state_dict(
-                torch.load(dec_weights, map_location='cuda:0'), strict=False)
-    net_encoder = net_encoder.cuda(0)
-    net_decoder = net_decoder.cuda(0)
-    auxi = net_encoder(img.cuda(0),return_feature_maps=True)
-    pred = net_decoder(auxi, segSize=(480, 640))
-    scores = torch.zeros(1, 150, 480, 640).cuda(0) # 150 num class
-    scores = scores + pred
-    _, pred = torch.max(scores, dim=1)
-    pred = as_numpy(pred.squeeze(0).cpu())
-    pred = (pred==3)*pred
-    pred = np.uint8(pred)
-    BEV_img = geompu.create_BEV_image(pred)
-    throttle_map, trajectory_BEV, trajectory_img = geompu.evaluate_grid(BEV_img)
-    print("Throttle map:")
-    print(throttle_map)
-    print("Trajectory BEV")
-    print(trajectory_BEV)
-    print("Trajectory img")
-    print(trajectory_img)
-    for i in range(7):
-        cv2.circle(pred, (int(trajectory_img[0,i]), int(trajectory_img[1,i])), 5, (0,255,0),5)   
-    plt.imshow(pred)
-    plt.show()
-"""
+# HW
 output_pin = "GPIO_PE6"
 if output_pin is None:
     raise Exception('PWM not supported on this board')
@@ -102,7 +42,6 @@ def set_movement(in1, in2, direction='f'):
     else:
         GPIO.output(in1, GPIO.HIGH)
         GPIO.output(in2, GPIO.LOW)
-
 
 # set pin as an output pin with optional initial state of HIGH
 GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.LOW)
@@ -124,23 +63,85 @@ i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
 kit = ServoKit(channels=16, i2c=i2c_bus0)
 # kit[0] is the bottom servo
 # kit[1] is the top servo
-#print("Done initializing")
+set_movement(in1_pin, in2_pin, 'f')
+kit.servo[0].angle = int(18)
+p.ChangeDutyCycle(0)
+print("Done initializing")
 
-steering = [18,0,36]
+# Disconnect
+print("Aneu desconectant")
+for j in range(45):
+    print(45-j)
+    time.sleep(1)
+
+# LED BLINKING
+for i in range(10):
+    if i%2==0:
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+    else:
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+
+torch.cuda.set_device(0)
+
+cam = Camera()
+grid_params = {'xw':0.6, 'yw':+0.25, 'length_x':1.0, 'length_y':0.50, 'num_x':7,'num_y':7}
+geompu = GeomPU(cam.get_K(), cam.get_Rt(),grid_params)
+
 try:
-    val = 90
-    for i in range(3):
-        kit.servo[0].angle = steering[i]
-        p.ChangeDutyCycle(val)
-        time.sleep(0.8)
-        set_movement(in1_pin, in2_pin, 'b')
-        p.ChangeDutyCycle(val)
-        time.sleep(0.8)
-        set_movement(in1_pin, in2_pin, 'f')
+
+    iterations = 0
+    while(iterations<100):
+        image = cam.get_frame_cv2()
+        #image_path = "/home/dlinano/Pictures/Terrinus/16.jpeg"
+        #image = cv2.imread(image_path)
+        #cv2.imwrite('/home/dlinano/Pictures/pic1.jpeg', image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        img = Image.fromarray(image)
+        ori_width, ori_height = img.size
+
+        # image transform, to torch float tensor 3xHxW
+        img = imresize(img, (480,640))
+        img = img_transform(img)
+        img = torch.unsqueeze(img, 0)
+        #print("img size = " + str(img.size()))
+
+        with torch.no_grad():
+            orig_mobilenet = mobilenet.__dict__['mobilenetv2'](pretrained=False)
+            net_encoder = MobileNetV2Dilated(orig_mobilenet, dilate_scale=8)
+            enc_weights = "/home/dlinano/maweights/ade20k-mobilenetv2dilated-c1_deepsup/encoder_epoch_20.pth"
+            net_encoder.load_state_dict(torch.load(enc_weights, map_location='cuda:0'), strict=False)
+    
+            net_decoder = C1DeepSup(num_class=150,fc_dim=320,use_softmax=True)
+            dec_weights = "/home/dlinano/maweights/ade20k-mobilenetv2dilated-c1_deepsup/decoder_epoch_20.pth"
+            net_decoder.load_state_dict(torch.load(dec_weights, map_location='cuda:0'), strict=False)
+            net_encoder = net_encoder.cuda(0)
+            net_decoder = net_decoder.cuda(0)
+            auxi = net_encoder(img.cuda(0),return_feature_maps=True)
+            pred = net_decoder(auxi, segSize=(480, 640))
+            scores = torch.zeros(1, 150, 480, 640).cuda(0) # 150 num class
+            scores = scores + pred
+            _, pred = torch.max(scores, dim=1)
+            pred = as_numpy(pred.squeeze(0).cpu())
+            pred = (pred==3)*pred
+            pred = np.uint8(pred)
+            BEV_img = geompu.create_BEV_image(pred)
+            throttle_map, trajectory_BEV, trajectory_img = geompu.evaluate_grid(BEV_img)
+            throttle_weights = np.array([0.1,0.1,0.1,0.1,0.2,0.2,0.2])
+            max_throt_val = 75
+            final_throttle = (max_throt_val/grid_params['num_y'])*np.sum(throttle_map*throttle_weights)
+            steering_weights = np.array([0.1,0.1,0.1,0.1,0.2,0.2,0.2])
+            h,w = BEV_img.shape
+            steering_ys = (trajectory_BEV[1,:]/w)*36.0
+            final_steering = np.sum(steering_weights*steering_ys)
+            kit.servo[0].angle = int(final_steering)
+            p.ChangeDutyCycle(final_throttle)
+            iterations = iterations + 1
         
 finally:
     p.stop()
     GPIO.output(in2_pin, GPIO.LOW)
     GPIO.cleanup()
 
-"""
